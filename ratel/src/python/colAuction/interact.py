bde3e8c9823334deda197ed35cced0782078f08b
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2022-05-24 15:38:50 +0800

    colAuction upd

--- ratel/src/python/colAuction/interact.py
+++ ratel/src/python/colAuction/interact.py
@@ -60,6 +60,7 @@ def submitBids(appContract,colAuctionId,price,amt,account):
     while True:
         time.sleep(1)
         status = appContract.functions.status(colAuctionId).call()
+        
         if status-2 > cur_bidcnt:
             bids_cnt[colAuctionId-1] = status-2
             return
