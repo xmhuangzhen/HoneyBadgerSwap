f6991e0b6a24909fc7163116f649d12123ba72df
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2022-06-02 13:11:37 +0800

    colAuction benchmark 0206

--- ratel/src/python/colAuction/interact.py
+++ ratel/src/python/colAuction/interact.py
@@ -84,7 +84,7 @@ if __name__=='__main__':
     tmp_list = file.readline().strip('\n').split(',')
 
     start_t = int(tmp_list[0]); StartPrice = int(tmp_list[1])
-    FloorPrice = 1700; totalAmt = int(float(tmp_list[3])*100)
+    FloorPrice = 2000; totalAmt = int(float(tmp_list[3])*100)
     creator_addr = Web3.toChecksumAddress(tmp_list[4])
     aucapp_addr = Web3.toChecksumAddress(tmp_list[5])
     # print(start_t,StartPrice,totalAmt,app_addr)
